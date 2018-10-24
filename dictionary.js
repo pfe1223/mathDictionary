28f855fde64aed4cf621b79ffa789042ac1ed614
pfe1223 <patrick.ester@gmail.com>
2018-10-24 14:24:00 -0700

    Add Images
    Add test images.

--- dictionary.js
+++ dictionary.js
@@ -7,63 +7,63 @@ const data = [
     "frenchdescription": "la meme chose",
     "englishword": "Symmetrical",
     "englishdescription": "same on both sides",
-    "image": "image1"
+    "image": "image01"
   },
   {
     "frenchword": "l'axe",
     "frenchdescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "englishword": "axis",
     "englishdescription": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "image": "image2"
+    "image": "image02"
   },
   {
     "frenchword": "égalité",
     "frenchdescription": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
     "englishword": "equality",
     "englishdescription": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "image": "image3"
+    "image": "image03"
   },
   {
     "frenchword": "multiplication",
     "frenchdescription": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
     "englishword": "multiplication",
     "englishdescription": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
-    "image": "image4"
+    "image": "image04"
   },
   {
     "frenchword": "soustraction",
     "frenchdescription": "Et harum quidem rerum facilis est et expedita distinctio.",
     "englishword": "substraction",
     "englishdescription": "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-    "image": "image5"
+    "image": "image05"
   },
   {
     "frenchword": "superficie",
     "frenchdescription": "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
     "englishword": "area",
     "englishdescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "image": "image6"
+    "image": "image06"
   },
   {
     "frenchword": "volume",
     "frenchdescription": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
     "englishword": "volume",
     "englishdescription": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-    "image": "image7"
+    "image": "image07"
   },
   {
     "frenchword": "racine carrée",
     "frenchdescription": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
     "englishword": "square root",
     "englishdescription": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "image": "image8"
+    "image": "image08"
   },
   {
     "frenchword": "longeur",
     "frenchdescription": "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
     "englishword": "length",
     "englishdescription": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "image": "image9"
+    "image": "image09"
   },
   {
     "frenchword": "largeur",
