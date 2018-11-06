b684ef6318a0368d73a4347e97862cc61f2a8131
pfe1223 <patrick.ester@gmail.com>
2018-11-06 10:32:05 -0800

    Use Myjson API for data
    Use the Myjson service to host the data in an accessible way through their API.

--- dictionary.js
+++ dictionary.js
@@ -1,118 +1,13 @@
 'use strict';
 
-//data to be displayed
-const data = [
-  {
-    "objID" : 1,
-    "frenchword": "symmetrique",
-    "frenchdescription": "la meme chose",
-    "englishword": "symmetrical",
-    "englishdescription": "same on both sides",
-    "image": "image01"
-  },
-  {
-    "objID" : 2,
-    "frenchword": "l'axe",
-    "frenchdescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "englishword": "axis",
-    "englishdescription": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "image": "image02"
-  },
-  {
-    "objID" : 3,
-    "frenchword": "égalité",
-    "frenchdescription": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-    "englishword": "equality",
-    "englishdescription": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "image": "image03"
-  },
-  {
-    "objID" : 4,
-    "frenchword": "multiplication",
-    "frenchdescription": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-    "englishword": "multiplication",
-    "englishdescription": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
-    "image": "image04"
-  },
-  {
-    "objID" : 5,
-    "frenchword": "soustraction",
-    "frenchdescription": "Et harum quidem rerum facilis est et expedita distinctio.",
-    "englishword": "substraction",
-    "englishdescription": "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-    "image": "image05"
-  },
-  {
-    "objID" : 6,
-    "frenchword": "superficie",
-    "frenchdescription": "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
-    "englishword": "area",
-    "englishdescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "image": "image06"
-  },
-  {
-    "objID" : 7,
-    "frenchword": "volume",
-    "frenchdescription": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-    "englishword": "volume",
-    "englishdescription": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-    "image": "image07"
-  },
-  {
-    "objID" : 8,
-    "frenchword": "racine carrée",
-    "frenchdescription": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
-    "englishword": "square root",
-    "englishdescription": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "image": "image08"
-  },
-  {
-    "objID" : 9,
-    "frenchword": "longeur",
-    "frenchdescription": "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    "englishword": "length",
-    "englishdescription": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "image": "image09"
-  },
-  {
-    "objID" : 10,
-    "frenchword": "largeur",
-    "frenchdescription": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    "englishword": "width",
-    "englishdescription": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "image": "image10"
-  },
-  {
-    "objID" : 11,
-    "frenchword": "hauteur",
-    "frenchdescription": "Et harum quidem rerum facilis est et expedita distinctio.",
-    "englishword": "height",
-    "englishdescription": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-    "image": "image11"
-  },
-  {
-    "objID" : 12,
-    "frenchword": "multiples",
-    "frenchdescription": "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
-    "englishword": "multiples",
-    "englishdescription": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
-    "image": "image12"
-  },
-  {
-    "objID" : 13,
-    "frenchword": "facteur",
-    "frenchdescription": "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.",
-    "englishword": "factor",
-    "englishdescription": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    "image": "image13"
-  }
-]
+const apiUrl = 'https://api.myjson.com/bins/1bfvwm'
 
 class MathDictionary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: "", //search term entered by the user
+      words: [], //words that make up the dictionary
+      search: "", //default search term
       obj: "", //object ID from the JSON objects
       language: "french", //choice between "engligh" and "french"
       view: "list" //choice betwen "list" and "definition"
@@ -135,6 +30,15 @@ class MathDictionary extends React.Component {
     this.showLanguageButtons = this.showLanguageButtons.bind(this) //display the "French" and "English" language buttons
     this.showListOfWords = this.showListOfWords.bind(this) //display the list of words
     this.showTitle = this.showTitle.bind(this) //show <h1> title on page
+  }
+
+  componentWillMount() {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(result => this.setState({
+        words: result
+      }))
+      .catch(error => error);
   }
 
   //sets the stat variable "language" to "french"
@@ -177,9 +81,9 @@ class MathDictionary extends React.Component {
   getWords() {
     let words
     if(this.state.language === "french"){
-      words = data.map(word => word.frenchword.toLowerCase())
+      words = this.state.words.map(word => word.frenchword.toLowerCase())
     } else if(this.state.language === "english") {
-      words = data.map(word => word.englishword.toLowerCase())
+      words = this.state.words.map(word => word.englishword.toLowerCase())
     }
     return words.sort()
   }
@@ -230,10 +134,11 @@ class MathDictionary extends React.Component {
   //changes state variable "view" to "definition"
   clickWord(event) {
     const word = event.target.value
+    const data = this.state.words
     let object
     if(this.state.language === "french") {
       for(let i = 0; i < data.length; i++) {
-        if(data[i].frenchword.toLowerCase() === word) {
+        if(this.state.words[i].frenchword.toLowerCase() === word) {
           object = data[i].objID
         }
       }
@@ -255,6 +160,7 @@ class MathDictionary extends React.Component {
   //shows both the English and French definitions of the selected word
   showDefinition() {
     let fDef, eDef
+    const data = this.state.words
     for (let i = 0; i < data.length; i++) {
       if(data[i].objID === this.state.obj) {
         fDef = data[i].frenchdescription
@@ -272,6 +178,7 @@ class MathDictionary extends React.Component {
   //shows the selected word (in French and English) on the definition page
   showWords() {
     let fWord, eWord
+    const data = this.state.words
     for (let i = 0; i < data.length; i++) {
       if(data[i].objID === this.state.obj) {
         fWord = data[i].frenchword
@@ -284,6 +191,7 @@ class MathDictionary extends React.Component {
   //show the image of the selected word on the definition page
   showImage() {
     let img
+    const data = this.state.words
     for (let i = 0; i < data.length; i++) {
       if(data[i].objID === this.state.obj) {
         img = "/assets/" + data[i].image + ".png"
@@ -348,6 +256,7 @@ class MathDictionary extends React.Component {
   }
 
   render() {
+    //console.log(this.state.words)
     if (this.state.view === "list") {
         return this.listView() //show the list of words
     } else if (this.state.view === "definition") {
